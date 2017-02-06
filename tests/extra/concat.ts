6e693896c72fda4143d607e2b3996797fb822d62
Linas Juškevičius <linas@prodigito.lt>
2017-02-06 13:14:42 +0200

    Failing tests concatting Promise + EventEmitter.

--- tests/extra/concat.ts
+++ tests/extra/concat.ts
@@ -1,6 +1,9 @@
 /// <reference path="../../typings/globals/mocha/index.d.ts" />
 /// <reference path="../../typings/globals/node/index.d.ts" />
+import {Promise} from 'es6-promise';
+import {EventEmitter} from 'events';
 import xs from '../../src/index';
+import fromEvent from '../../src/extra/fromEvent';
 import concat from '../../src/extra/concat';
 import * as assert from 'assert';
 
@@ -58,5 +61,57 @@ describe('concat (extra)', () => {
         done();
       },
     });
+  });
+
+  it('should sequentially concatenate a promise stream with an EventEmitter stream, out of order', (done: any) => {
+    let promiseResolver: (value: number) => void;
+    const eventEmitter = new EventEmitter();
+
+    const stream1 = xs.fromPromise(new Promise((resolve) => {
+      promiseResolver = resolve;
+    }));
+    const stream2 = fromEvent(eventEmitter, 'test');
+    const stream = concat(stream1, stream2);
+    const expected = [0, 1];
+
+    stream.addListener({
+      next: (x: number) => {
+        assert.equal(x, expected.shift());
+      },
+      error: (err: Error) => done(err),
+      complete: () => {
+        assert.equal(expected.length, 0);
+        done();
+      },
+    });
+
+    setTimeout(() => eventEmitter.emit('test', 1));
+    setTimeout(() => promiseResolver(0));
+  });
+
+  it('should sequentially concatenate a promise stream with an EventEmitter stream, in order', (done: any) => {
+    let promiseResolver: (value: number) => void;
+    const eventEmitter = new EventEmitter();
+
+    const stream1 = xs.fromPromise(new Promise((resolve) => {
+      promiseResolver = resolve;
+    }));
+    const stream2 = fromEvent(eventEmitter, 'test');
+    const stream = concat(stream1, stream2);
+    const expected = [0, 1];
+
+    stream.addListener({
+      next: (x: number) => {
+        assert.equal(x, expected.shift());
+      },
+      error: (err: Error) => done(err),
+      complete: () => {
+        assert.equal(expected.length, 0);
+        done();
+      },
+    });
+
+    setTimeout(() => promiseResolver(0));
+    setTimeout(() => eventEmitter.emit('test', 1));
   });
 });
